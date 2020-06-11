c67e346875f08fa33e93f138491698e515a4778d
Dragos Cont <dragoscont@live.com>
2020-06-11 17:52:46 +0300

    added classes for the exercise

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Sum sum = new Sum();
+        sum.output();
+
+        System.out.println("\n");
+        Substraction substraction = new Substraction();
+        substraction.output();
+
+        System.out.println("\n");
+        Multiplication multiplication = new Multiplication();
+        multiplication.output();
+    }
+
+
+
+}
